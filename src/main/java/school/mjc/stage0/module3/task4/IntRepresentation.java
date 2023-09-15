60fd3add19a52a67209c683e98704c29032ae74a
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-15 15:44:56 +0600

    Update IntRepresentation.java

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int a, b;
+        System.out.println((9*a*a-5*a+2+a-7)*((a+b-4*a*b)/2));
+        
     }
+    
 }
