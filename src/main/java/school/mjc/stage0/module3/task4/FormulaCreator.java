4bccad02f4ef5df74f27f53074caa2197a90aca5
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-15 15:52:28 +0600

    Update FormulaCreator.java

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a, b;
+        int a=2, b=3;
         System.out.println((9*a*a-5*a+2+a-7)*((a+b-4*a*b)/2));
     }
     
