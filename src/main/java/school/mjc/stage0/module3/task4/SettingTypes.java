45e5c567076a78f58085825a92b32274631d9802
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-15 15:52:06 +0600

    Update SettingTypes.java

--- src/main/java/school/mjc/stage0/module3/task4/SettingTypes.java
+++ src/main/java/school/mjc/stage0/module3/task4/SettingTypes.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.module3.task4;
 
 public class SettingTypes {
     public static void main(String[] args) {
-        int first = 10_000_000_000L;
+        long int first = 10_000_000_000L;
         char second = 'a';
         boolean third = 5 > 9;
         double forth = 12.90434323324342;
